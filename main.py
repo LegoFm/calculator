63e2f13f9c184c4b725f9ad9291371cbc5fbd2eb
Grebenyuk Konstantin <grebenyuk@nwtn-group.com>
2022-11-29 03:53:22 +0300

    bruh

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import json
 import math
 
+# Реализация с загрузкой ответа по методу parameters/operation
 with open("input/text.json", "r") as file:
     data = json.load(file)
 
+# Создание словаря для заполнения параметрами калькулятора
+check = dict()
 
 # Функция для работы со значениями вне блоков из файла
 def decaps(x):
@@ -22,29 +25,40 @@ def decapsCommissionInfo(z):
     func = float(data["commissionInfo"][z])
     return func
 
+# Обращения по def = decaps
 lotSize = decaps("lotSize")
 marketInsuranceValue = decaps("marketInsuranceValue")
 
+# Обращения по def = decapsMargin
 fundsAvailable = decapsMargin("fundsAvailable")
 discountShort = decapsMargin("discountShort")
 discountLong = decapsMargin("discountLong")
 plannedPosition = decapsMargin("plannedPosition")
 
+# Обращения по def = decapsCommissionInfo
 minValue = decapsCommissionInfo("minValue")
 valueInPercent = decapsCommissionInfo("valueInPercent")
 
+# Получение даты вне parameters/operation
 price = float(input("Цена заявки"))
-discountCheck = input("Short = (S) Long = (L)")
-if (discountCheck = "S"):
-    discount = discountShort
-elif (discountCheck = "L"):
-    discount = discountLong
+Crossrate = float(input("Crossrate"))
+# Заворачиваем значения в параметры словаря check
+check['discount'] = input(" Используем discountShort = (S) или discountLong = (L)")
+check['direction'] = input("По направлению (1) или против направления (2)")
 
+# Заводим значение комиссии в словарь check
+if ( fundsAvailable / (1 + valueInPercent)) - (minValue / valueInPercent * (1 + valueInPercent)) >= 0:
+    check['commission'] = 'percentage'
+else:
+    check['commission'] = 'natural'
 
- def answer(formula):
-     match formula:
-         case 0:
-             return math.floor(decapsMargin("fundsAvailable")) / (price * (1 + marketInsuranceValue) * lotSize * discount * (1 + valueInPercent))
-
-
-formula = ( decapsMargin("fundsAvailable") / (1 + decapsCommissionInfo("valueInPercent"))) - (decapsCommissionInfo("minValue") / decapsCommissionInfo("valueInPercent") * (1 + decapsCommissionInfo("valueInPercent")))
+ def main():
+     match check:
+         case {'discount': 'L', 'direction': '1', 'commission': 'percentage'}:
+             (math.floor(fundsAvailable) / (price * (1 + marketInsuranceValue) * lotSize * discountLong * (1 + valueInPercent)))
+         case {'discount': 'L', 'direction': '1', 'commission': 'natural'}:
+             (math.floor(fundsAvailable) - minValue) / (price * (1 + marketInsuranceValue) * lotSize * discountLong)
+         case {'discount': 'S', 'direction': '1', 'commission': 'percentage'}:
+             (math.floor(fundsAvailable) / (price * (1 + marketInsuranceValue) * lotSize * discountShort * (1 + valueInPercent)))
+         case {'discount': 'S', 'direction': '1', 'commission': 'natural'}:
+             (math.floor(fundsAvailable) - minValue) / (price * (1 + marketInsuranceValue) * lotSize * discountShort)
