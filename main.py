988b2c80609f54b439eb9dbe809ba011d4c5edb3
Grebenyuk Konstantin <grebenyuk@nwtn-group.com>
2022-12-15 01:43:59 +0300

    1

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ with open("input/text.json", "r") as file:
 # Создание словаря для заполнения параметрами калькулятора
 check = dict()
 
+
 # Функция для работы со значениями вне блоков из файла
 def decaps(x):
     func = data[x]
@@ -26,47 +27,61 @@ def decapsCommissionInfo(z):
     func = float(data["commissionInfo"][z])
     return func
 
-# Обращения по def = decaps
+
+# Обращения по функции decaps
 lotSize = decaps("lotSize")
 marketInsuranceValue = decaps("marketInsuranceValue")
 
-# Обращения по def = decapsMargin
+# Обращения по функции decapsMargin
 fundsAvailable = decapsMargin("fundsAvailable")
 discountShort = decapsMargin("discountShort")
 discountLong = decapsMargin("discountLong")
 plannedPosition = decapsMargin("plannedPosition")
 
-# Обращения по def = decapsCommissionInfo
+# Обращения по функции decapsCommissionInfo
 minValue = decapsCommissionInfo("minValue")
 valueInPercent = decapsCommissionInfo("valueInPercent")
 
 # Получение даты вне parameters/operation
 price = float(input("Цена заявки"))
 Crossrate = float(input("Crossrate"))
+
 # Заворачиваем значения в параметры словаря check
 check['discount'] = input(" Используем discountShort = (S) или discountLong = (L)")
 check['direction'] = input("По направлению (1) или против направления (2)")
 
 # Заводим значение комиссии в словарь check
-if ( fundsAvailable / (1 + valueInPercent)) - (minValue / valueInPercent * (1 + valueInPercent)) >= 0:
+if (fundsAvailable / (1 + valueInPercent)) - (minValue / valueInPercent * (1 + valueInPercent)) >= 0:
     check['commission'] = 'percentage'
 else:
     check['commission'] = 'natural'
-
 
 
 def main(check):
     print(check)
     print(type(check))
     match check:
-     case {'discount': 'L', 'direction': '1', 'commission': 'percentage'}:
-         func = (math.floor(fundsAvailable) / (price * (1 + marketInsuranceValue) * lotSize * discountLong * (1 + valueInPercent)))
-     case {'discount': 'L', 'direction': '1', 'commission': 'natural'}:
-         func = (math.floor(fundsAvailable) - minValue) / (price * (1 + marketInsuranceValue) * lotSize * discountLong)
-     case {'discount': 'S', 'direction': '1', 'commission': 'percentage'}:
-         func = (math.floor(fundsAvailable) / (price * (1 + marketInsuranceValue) * lotSize * discountShort * (1 + valueInPercent)))
-     case {'discount': 'S', 'direction': '1', 'commission': 'natural'}:
-         func = (math.floor(fundsAvailable) - minValue) / (price * (1 + marketInsuranceValue) * lotSize * discountShort)
+        case {'discount': 'L', 'direction': '1', 'commission': 'percentage'}:
+            func = (math.floor(fundsAvailable) / (
+                    price * (1 + marketInsuranceValue) * lotSize * discountLong * (1 + valueInPercent)))
+        case {'discount': 'L', 'direction': '1', 'commission': 'natural'}:
+            func = (math.floor(fundsAvailable) - minValue) / (
+                    price * (1 + marketInsuranceValue) * lotSize * discountLong)
+        case {'discount': 'S', 'direction': '1', 'commission': 'percentage'}:
+            func = (math.floor(fundsAvailable) / (
+                    price * (1 + marketInsuranceValue) * lotSize * discountShort * (1 + valueInPercent)))
+        case {'discount': 'S', 'direction': '1', 'commission': 'natural'}:
+            func = (math.floor(fundsAvailable) - minValue) / (
+                    price * (1 + marketInsuranceValue) * lotSize * discountShort)
+        case {'discount': 'L', 'direction': '2', 'commission': 'percentage'}:
+            func = -- дописать
+        case {'discount': 'L', 'direction': '2', 'commission': 'natural'}:
+            func = -- дописать
+        case {'discount': 'S', 'direction': '2', 'commission': 'percentage'}:
+            func = -- дописать
+        case {'discount': 'S', 'direction': '2', 'commission': 'natural'}:
+            func = -- дописать
     return print(func)
+
 
 main(check)
